c2f44f1cf2010c393d38429b5ae15225879ed15e
DESKTOP-5H96181\Kaltome <Kaltomesss@gmail.com>
2019-07-10 18:54:54 +0800

    Add Account System

---
+++ No9Gallery/No9Gallery/Models/Chargelist.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace No9Gallery.Models
+{
+    public class Chargelist
+    {
+        [Display(Name = "order no")]
+        public string order_no { get; set; }
+
+        public string user_ID { get; set; }
+        public int amount { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime time { get; set; }
+        public string content { get; set; }
+
+    }
+}
---
+++ No9Gallery/No9Gallery/Models/ReviseInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace No9Gallery.Models
+{
+    public class ReviseInfo
+    {
+        public string UserID { get; set; }
+        public string NewUserName { get; set; }
+        public string NewPassword { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string NewIntroduction { get; set; }
+    }
+}
